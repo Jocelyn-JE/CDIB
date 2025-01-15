0e9d1a93075f92658b68e08f2149423d7c235dc1
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2025-01-15 10:57:26 +0100

    ♻️ Moved api calling functions from `index.js` to `apiCalls.js`

---
+++ src/apiCalls.js
@@ -0,0 +1,72 @@
+const axios = require('axios');
+const https = require('https');
+const fs = require('fs');
+require('dotenv').config();
+
+// Disable SSL verification
+const agent = new https.Agent({
+    rejectUnauthorized: false,
+});
+const auth_header = { headers: { Authorization: `Bearer ${process.env.CRAFTY_API_TOKEN}` }, httpsAgent: agent };
+
+const API_URL = 'https://' + process.env.CRAFTY_API_URL + ':8443/api/v2/servers';
+const SERVER_ID = process.env.CRAFTY_SERVER_ID;
+
+export async function sendServerAction(action, sucessmsg, errormsg) {
+    try {
+        const response = await axios.post(`${API_URL}/${SERVER_ID}/action/${action}`, {}, auth_header);
+
+        if (response.data.status === 'ok') {
+            return sucessmsg;
+        }
+        else {
+            return errormsg + JSON.stringify(response.data);
+        }
+    }
+    catch (error) {
+        console.error(error);
+        return `Error performing action '${action}': ${error.message}`;
+    }
+}
+
+export async function sendServerSTDIn(cmd, sucessmsg, errormsg) {
+    try {
+        const response = await axios.post(`${API_URL}/${SERVER_ID}/stdin`, cmd, auth_header);
+
+        if (response.data.status === 'ok') {
+            return sucessmsg;
+        }
+        else {
+            return errormsg + JSON.stringify(response.data);
+        }
+    }
+    catch (error) {
+        console.error(error);
+        return `Error performing command '${cmd}': ${error.message}`;
+    }
+}
+
+export async function getServerStats(errormsg) {
+    try {
+        const response = await axios.get(`${API_URL}/${SERVER_ID}/stats`, auth_header);
+
+        fs.writeFile('./api_response.json', JSON.stringify(response.data, null, 2), (err) => {
+            if (err) {
+                console.error('Error writing to file:', err);
+                return;
+            }
+            console.log('Data successfully saved to file:', './api_response.json');
+        });
+
+        if (response.data.status === 'ok') {
+            return response;
+        }
+        else {
+            return errormsg;
+        }
+    }
+    catch (error) {
+        console.error(error);
+        return `Error getting stats: ${error.message}`;
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,90 +1,13 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const axios = require('axios');
-const https = require('https');
-require('dotenv').config(); // To load environment variables
-
-// Initialize a custom agent that ignores SSL certificate validation
-const agent = new https.Agent({  
-    rejectUnauthorized: false  // Disable SSL verification
-});
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
+import * as api from './apiCalls.js';
+require('dotenv').config();
 
 // Initialize bot client
 const client = new Client({
     intents: [GatewayIntentBits.Guilds],
 });
-
-const API_URL = 'https://'+process.env.CRAFTY_API_URL+':8443/api/v2/servers';
-const SERVER_ID = process.env.CRAFTY_SERVER_ID;  // Replace with actual server ID
-
-// Helper function to send server control commands
-async function sendServerAction(action, sucessmsg, errormsg) {
-    try {
-        const response = await axios.post(`${API_URL}/${SERVER_ID}/action/${action}`, {}, {
-            headers: {
-                Authorization: `Bearer ${process.env.CRAFTY_API_TOKEN}`,
-            },
-            httpsAgent: agent  // Use the custom agent
-        });
-
-        if (response.data.status === 'ok') {
-            return sucessmsg;
-        } else {
-            return errormsg + JSON.stringify(response.data);
-        }
-    } catch (error) {
-        console.error(error);
-        return `Error performing action '${action}': ${error.message}`;
-    }
-}
-
-async function sendServerSTDIn(cmd, sucessmsg, errormsg) {
-    try {
-        const response = await axios.post(`${API_URL}/${SERVER_ID}/stdin`, cmd,
-        {
-            headers: {
-                Authorization: `Bearer ${process.env.CRAFTY_API_TOKEN}`,
-            },
-            httpsAgent: agent  // Use the custom agent
-        });
-
-        if (response.data.status === 'ok') {
-            return sucessmsg;
-        } else {
-            return errormsg + JSON.stringify(response.data);
-        }
-    } catch (error) {
-        console.error(error);
-        return `Error performing command '${cmd}': ${error.message}`;
-    }
-}
-
-async function getServerStats(errormsg) {
-    try {
-        const response = await axios.get(`${API_URL}/${SERVER_ID}/stats`,
-        {
-            headers: {
-                Authorization: `Bearer ${process.env.CRAFTY_API_TOKEN}`,
-            },
-            httpsAgent: agent  // Use the custom agent
-        });
-
-        if (response.data.status === 'ok') {
-            msg = "Server status: "
-            if (response.data.data.running == true) {
-                msg += 'ON :green_circle:\n'
-            } else {
-                msg += 'OFF :red_circle:\n'
-            }
-            msg += 'Players online: **' + response.data.data.online + '/' + response.data.data.max + '**'
-            return msg;
-        } else {
-            return errormsg + JSON.stringify(response.data);
-        }
-    } catch (error) {
-        console.error(error);
-        return `Error getting stats: ${error.message}`;
-    }
-}
+// Bot login
+client.login(process.env.DISCORD_BOT_TOKEN);
 
 // Register slash commands and handle interactions
 client.on('interactionCreate', async (interaction) => {
@@ -93,54 +16,62 @@ client.on('interactionCreate', async (interaction) => {
     const { commandName } = interaction;
 
     // Check if the user has the required role to use the stopserver command
-    const requiredRole = process.env.ADMIN_ROLE_ID; // Replace with the role name or ID you want to allow
-    const hasRole = interaction.member.roles.cache.has(requiredRole); // Check if the member has the role
+    const adminRole = process.env.ADMIN_ROLE_ID;
+    const hasRole = interaction.member.roles.cache.has(adminRole);
 
     // Handle 'startserver' command
-    if (commandName === 'start') {
-        const response = await sendServerAction('start_server', 'Serveur démarré!', 'Le démarrage à échoué: ');
+    if (commandName === 'start' || commandName === 'start_test') {
+        const response = await api.sendServerAction('start_server', 'Serveur démarré!', 'Le démarrage à échoué: ');
         await interaction.reply(response);
     }
 
     // Handle 'stats' command
-    if (commandName === 'stats') {
-        const response = await getServerStats('La récupération des statistiques à échoué: ');
-        await interaction.reply(response);
+    if (commandName === 'stats' || commandName === 'stats_test') {
+        const response = await api.getServerStats('La récupération des statistiques à échoué @<328210913645559809>');
+        msg = 'Server status: ';
+        if (response.data.data.running === true) {
+            msg += 'ON :green_circle:\n';
+            const date = new Date(response.data.data.started);
+            const epochTimestamp = Math.floor(date.getTime() / 1000) + 3600;
+            msg += `Started <t:${epochTimestamp}:R>\n`;
+            msg += 'Players online: **' + response.data.data.online + '/' + response.data.data.max + '**';
+            client.user.setActivity('mc.firebuildworld.ovh', {
+                type: ActivityType.Playing,
+            });
+        }
+        else {
+            msg += 'OFF :red_circle:\n';
+        }
+        await interaction.reply(msg);
     }
 
     // Handle 'stopserver' command
-    if (commandName === 'stop') {
+    if (commandName === 'stop' || commandName === 'stop_test') {
         if (!hasRole) {
             return await interaction.reply('You do not have permission to stop the server.');
-        }
-
-        const response = await sendServerAction('stop_server', 'Serveur éteint!', "L'arrêt à échoué: ");
+        };
+        const response = await api.sendServerAction('stop_server', 'Serveur éteint!', 'L\'arrêt à échoué: ');
         await interaction.reply(response);
     }
 
-    // Handle 'stopserver' command
-    if (commandName === 'restart') {
+    // Handle 'restart' command
+    if (commandName === 'restart' || commandName === 'restart_test') {
         if (!hasRole) {
             return await interaction.reply('You do not have permission to restart the server.');
-        }
-
+        };
         const response = await sendServerAction('restart_server', 'Serveur redémarré!', 'Le redémarrage à échoué: ');
         await interaction.reply(response);
     }
 
     // Handle 'run' command
-    if (commandName === 'run') {
+    if (commandName === 'run' || commandName === 'run_test') {
         if (!hasRole) {
             return await interaction.reply('You do not have permission to run a command.');
         }
-
         // Get the 'command' option from the interaction
         const command = interaction.options.getString('command');
 
-        const response = await sendServerSTDIn(command, 'Commande: **/'+command+'**, envoyée', 'La commande à échoué: ');
+        const response = await api.sendServerSTDIn(command, 'Commande: **/' + command + '**, envoyée', 'La commande à échoué: ');
         await interaction.reply(response);
     }
 });
-
-// Bot login
-client.login(process.env.DISCORD_BOT_TOKEN);
