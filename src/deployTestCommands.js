d0b8540f148aab483110a2fc119bcb07d4efa2fc
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2025-01-15 12:25:52 +0100

    ✨ Added skipping of already existing function when testing

--- src/deployTestCommands.js
+++ src/deployTestCommands.js
@@ -7,10 +7,16 @@ const rest = new REST({ version: '10' }).setToken(botToken);
 (async () => {
     try {
         console.log('Started deploying guild-specific application (/) commands one by one.');
-
+        const existingCommands = await rest.get(Routes.applicationCommands(clientId));
+        const existingCommandNames = existingCommands.map(command => command.name);
         for (const command of commands) {
             try {
-                // Deploy each command to the test guild
+                // Skip command if it already exists
+                if (existingCommandNames.includes(command.name)) {
+                    console.log(`Command ${command.name} already exists. Skipping.`);
+                    continue;
+                }
+                // Deploy the command to the test guild
                 command.name += '_test';
                 await rest.post(Routes.applicationGuildCommands(clientId, guildId), { body: command });
                 console.log(`Successfully deployed command: ${command.name}`);
@@ -19,7 +25,6 @@ const rest = new REST({ version: '10' }).setToken(botToken);
                 console.error(`Error deploying command ${command.name}:`, error);
             }
         }
-
         console.log('Finished deploying all guild-specific commands.');
     }
     catch (error) {
