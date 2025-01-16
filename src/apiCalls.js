7cbc6b49cb84d29aa9cfa12d1dbbe92d5755c208
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2025-01-16 14:26:01 +0100

    🔥 Removed logging of api response when getting server stats

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -55,7 +55,6 @@ export async function getServerStats(errormsg) {
                 console.error('Error writing to file:', err);
                 return;
             }
-            console.log('Data successfully saved to file:', './api_response.json');
         });
 
         if (response.data.status === 'ok') {
